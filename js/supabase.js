9d8d89fe54e69794b55261a7ffb72a39ce08c1c5
RickHardBR <rickhhard@gmail.com>
2026-03-01 03:50:53 -0300

    teste de pull

---
+++ js/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://rlilxzkddywbgqdrmckb.supabase.co";
+const SUPABASE_KEY = "sb_publishable_PsOGBXW5yagei4WdMbA1fA_7pvNjJtv";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
